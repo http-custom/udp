ccd5ffec4484b191478cf094b128a3268ff3beda
http-custom <137656009+http-custom@users.noreply.github.com>
2024-03-13 23:39:46 -0300

    Add files via upload

--- verconectados.sh
+++ verconectados.sh
@@ -2,28 +2,28 @@
 
 while true; do
   echo "BIENVENIDO AL MENÚ PARA VER CONECTADOS"
-  echo "Seleccione una opción:"
-  echo "1. Ver conectados en el puerto 80"
-  echo "2. Ver conectados en el puerto 443"
-  echo "3. Salir"
+  echo "SELECCIONE UNA OPCIÓN:"
+  echo "1. VER CONECTADOS EN EL PUERTO 80"
+  echo "2. VER CONECTADOS EN EL PUERTO 8080"
+  echo "3. SALIR"
 
   read -p "Opción: " opcion
 
   case $opcion in
     1)
-      PSIPHON_PORT_8080=$(sudo netstat -tn | awk '$4 ~ /:8080$/ {print $5}' | cut -d: -f1 | sort | uniq -c | wc -l)
-      echo "CONEXIONES EN EL PUERTO 8080 DE PSIPHON: $PSIPHON_PORT_8080"
+      V2RAY_PORT_80=$(sudo netstat -tn | awk '$4 ~ /:80$/ {print $5}' | cut -d: -f1 | sort | uniq -c | wc -l)
+      echo "CONEXIONES EN EL PUERTO 80 DE V2RAY: $V2RAY_PORT_80"
       ;;
     2)
-      PSIPHON_PORT_80=$(sudo netstat -tn | awk '$4 ~ /:80$/ {print $5}' | cut -d: -f1 | sort | uniq -c | wc -l)
-      echo "CONEXIONES EN EL PUERTO 80 DE PSIPHON: $PSIPHON_PORT_80"
+      V2RAY_PORT_8080=$(sudo netstat -tn | awk '$4 ~ /:8080$/ {print $5}' | cut -d: -f1 | sort | uniq -c | wc -l)
+      echo "CONEXIONES EN EL PUERTO 8080 DE V2RAY: $V2RAY_PORT_8080"
       ;;
     3)
-      echo "Saliendo del script..."
+      echo "SALIENDO DEL SCRIPT..."
       break
       ;;
     *)
-      echo "Opción inválida"
+      echo "OPCIÓN INVÁLIDA"
       ;;
   esac
 
